64818d7b9691c4f2d0bd290745d27281d46c8db0
Waerden <waerden2018@gmail.com>
2020-02-02 17:50:49 -0500

    Basically implement the Bar, Section, ChordSection classes, still need to debug

--- pysong/transcribe.py
+++ pysong/transcribe.py
@@ -1,10 +1,13 @@
 
+from copy import copy, deepcopy as copy, deepcopy
+from pychord import Chord
+
 class Bar:
     """Kind of a replacement of the Time class. 
 
     :param: _content: list of tuples to be added in the Section class.
     """
-    def __init__(self, beat_per_measure, tick_per_beat):
+    def __init__(self, beat_per_measure = 4, tick_per_beat = 4):
         self.beat_per_measure = beat_per_measure
         self.tick_per_beat = tick_per_beat
         self._content = []
@@ -23,39 +26,81 @@ class Section:
 
         """
         self._table = {} 
+        self._num = 0
         pass
 
     def __repr__(self):
-        pass
-
-    def __add__(self, other):
-        """Add two sections to get a bigger section
-        """
-        pass
+        info = ""
+        for (num, bar) in self._table.item():
+           info += "".join(" "*(index % 4 == 0) + "_" for index in range(bar.beat_per_measure*bar.tick_per_beat)) + "\n"       
+        return info
 
     def add_bar(self, bar):
-        pass
+        if not isinstance(bar, Bar):
+            raise TypeError("Expected {} type instead of {}".format(type(Bar), type(bar)))
+        else:
+            self._table[self._num] = deepcopy(bar)
+            self._num += 1
 
     def add_bars(self, bar, num):
         for _ in range(num):
             self.add_bar(bar)
 
-    def del_bar(self, num):
-        """delete a bar from a section
-            TODO: be careful, delete a particular bar from a section is a wired operation, 
-            the numbering of the bars needs to be changed accordingly
+    def add_section(self, other):
+        if not isinstance(other, Section):
+            raise TypeError("Expected {} type instead of {} type".format(type(Section), type(other)))
+        for measure in other._table:
+            self.add_bar(other._table[measure])
+
+
+    def replace_bar(self, bar, bar_num):
         """
+        """
+        if bar_num in self._table:
+            self._table[bar_num] = deepcopy(bar) 
+
+    def del_last(self):
+        if self._num >= 1:
+            del self._table[self._num]
+            self._num -= 1
 
 ##song.py contains ChordTrack class, remember to delete it when it's necessary.
-class ChordSection:
-    def __init__(self, section):
+class ChordSection(Section):
+    def __init__(self, frame):
         """
+        TODO: might need deepcopy if we'll have more classes inherited from the Section class.
         :param: section: Section instance, gives the frame of a song section, e.g. verse of "How deep is your love"
         """
-        pass
+        if not isinstance(frame, Section):
+            raise TypeError("Expected to initiate ChordTrack by an instance of {} instead of {}".format(type(Section), type(frame)))
+        self._table = frame._table
+        self._num = frame._num
+
+    def insert(self, num, event):
+        """
+        :param: event, tuple of chord location and chord, e.g. ((1,4), Chord("E"))
+        """
+        assert num in self._frame, "Measure num out of range!"
+        self._frame[num]._content.append(event)
+
+    def fill_bar(self, num, events):
+        for event in events:
+            self.insert(event)
+
+
 
     def __repr__(self):
-        pass
+        info = ""
+        for (num, bar) in self._table.item():
+            chord_location = {}
+            for event in bar:
+                chord_location[(event[0][0]-1)*bar.beat_per_measure+(event[0][1]-1)*bar.tick_per_beat] = event[1]._name
+
+            info += "".join(" "*(index % 4 == 0) + (chord_location[index] if index in chord_location else "_") 
+                    for index in range(bar.beat_per_measure*bar.tick_per_beat))
+            info += "\n"
+        return info
+            
 
 class PercussionSection:
     def __init__(self, section):
@@ -74,4 +119,6 @@ class BassSection:
 #song.py contains another Song class, remember to delete it when it's necessary.
 class Song:
     def __init__(self):
+        self.structure = []
+        self.chordtrack = {}
         pass
